8f7ca92e073e116a08129dc52878c0f6d0b4c2cd
Benjamin Neff <benjamin@coding4coffee.ch>
2015-06-11 02:16:54 +0200

    rspec dummy test

--- lib/diaspora_federation/engine.rb
+++ lib/diaspora_federation/engine.rb
@@ -1,5 +1,9 @@
 module DiasporaFederation
   class Engine < ::Rails::Engine
     isolate_namespace DiasporaFederation
+
+    config.generators do |g|
+      g.test_framework :rspec
+    end
   end
 end
---
+++ spec/dummy_spec.rb
@@ -0,0 +1,6 @@
+describe "Dummy" do
+  # dummy test
+  it "should be true" do
+    expect(true).to be_truthy
+  end
+end
