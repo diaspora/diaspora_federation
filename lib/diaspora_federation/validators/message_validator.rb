699c74a8f08563ba745e091e12a113bd50ac0387
Benjamin Neff <benjamin@coding4coffee.ch>
2016-07-01 05:06:02 +0200

    validate message text

--- lib/diaspora_federation/validators/message_validator.rb
+++ lib/diaspora_federation/validators/message_validator.rb
@@ -7,6 +7,9 @@ module DiasporaFederation
       rule :author, %i(not_empty diaspora_id)
       rule :guid, :guid
       rule :conversation_guid, :guid
+
+      rule :text, [:not_empty,
+                   length: {maximum: 65_535}]
     end
   end
 end
--- spec/lib/diaspora_federation/validators/message_validator_spec.rb
+++ spec/lib/diaspora_federation/validators/message_validator_spec.rb
@@ -19,5 +19,13 @@ module DiasporaFederation
         let(:property) { :conversation_guid }
       end
     end
+
+    describe "#text" do
+      it_behaves_like "a property with a value validation/restriction" do
+        let(:property) { :text }
+        let(:wrong_values) { ["", "a" * 65_536] }
+        let(:correct_values) { ["a" * 65_535] }
+      end
+    end
   end
 end
