771a1559e3edaafa6b205a5d56f1104dfe698830
Benjamin Neff <benjamin@coding4coffee.ch>
2016-11-07 23:46:58 +0100

    Activate ruby 2.1 with rails 4.2 on travis again

---
+++ test/scripts/travis.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -x
+
+if [[ ${TRAVIS_RUBY_VERSION} == "2.1" ]]; then
+  # ruby 2.1
+  export NO_COVERAGE="true" # No coverage for rails 4, because controller specs are disabled
+  bundle exec rake rails4:test --trace
+else
+  # ruby >= 2.2
+  bundle exec rake --trace
+fi
