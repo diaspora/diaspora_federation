26cfe7090b94f5a87c75f254b605c48efe59aa8d
Benjamin Neff <benjamin@coding4coffee.ch>
2017-04-04 03:28:37 +0200

    Cleanup dummy rails app

--- test/dummy/app/controllers/discovery_controller.rb
+++
@@ -1,7 +0,0 @@
-class DiscoveryController < ApplicationController
-  def discovery
-    discovery = DiasporaFederation::Discovery::Discovery.new(params[:q])
-
-    render json: discovery.fetch_and_save
-  end
-end
