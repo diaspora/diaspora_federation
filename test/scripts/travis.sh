f08db93d74c87e1fb0e399232d88fccbf4e3dc47
Benjamin Neff <benjamin@coding4coffee.ch>
2017-04-24 00:05:46 +0200

    Fail when no-rails.Gemfile.lock contains a rails dependency
    Since all rails dependencies somehow depend on activesupport, we only
    need to check for activesupport :)

--- test/scripts/travis.sh
+++ test/scripts/travis.sh
@@ -3,6 +3,13 @@
 set -x
 
 if [[ ${BUNDLE_GEMFILE} =~ .*test/gemfiles/.*.Gemfile ]]; then
+  if [[ ${BUNDLE_GEMFILE} =~ .*/no-rails.Gemfile ]]; then
+    if grep activesupport "${BUNDLE_GEMFILE}.lock"; then
+      echo "ERROR! no-rails.Gemfile.lock contains rails dependency!"
+      exit 1
+    fi
+  fi
+
   # No coverage for other gemfiles, because some specs are disabled
   export NO_COVERAGE="true"
   bundle exec rake --trace
