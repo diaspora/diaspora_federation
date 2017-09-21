8535cfc1df7a4a01f2fd66685089d59fc66e4c0a
Benjamin Neff <benjamin@coding4coffee.ch>
2017-09-22 01:27:13 +0200

    Don't fetch the same entity twice in the same thread

--- lib/diaspora_federation/federation/fetcher.rb
+++ lib/diaspora_federation/federation/fetcher.rb
@@ -7,15 +7,9 @@ module DiasporaFederation
       # @param [Symbol, String] entity_type snake_case version of the entity class
       # @param [String] guid guid of the entity to fetch
       def self.fetch_public(author, entity_type, guid)
-        url = DiasporaFederation.callbacks.trigger(
-          :fetch_person_url_to, author, "/fetch/#{entity_name(entity_type)}/#{guid}"
-        )
-        response = HttpClient.get(url)
-        raise "Failed to fetch #{url}: #{response.status}" unless response.success?
-
-        magic_env_xml = Nokogiri::XML(response.body).root
-        magic_env = Salmon::MagicEnvelope.unenvelop(magic_env_xml)
-        Receiver::Public.new(magic_env).receive
+        type = entity_name(entity_type).to_s
+        raise "Already fetching ..." if fetching[type].include?(guid)
+        fetch_from_url(author, type, guid)
       rescue => e # rubocop:disable Lint/RescueWithoutErrorClass
         raise NotFetchable, "Failed to fetch #{entity_type}:#{guid} from #{author}: #{e.class}: #{e.message}"
       end
@@ -26,6 +20,23 @@ module DiasporaFederation
         raise DiasporaFederation::Entity::UnknownEntity, class_name unless Entities.const_defined?(class_name)
 
         class_name.gsub(/(.)([A-Z])/, '\1_\2').downcase
+      end
+
+      private_class_method def self.fetch_from_url(author, type, guid)
+        fetching[type] << guid
+
+        url = DiasporaFederation.callbacks.trigger(:fetch_person_url_to, author, "/fetch/#{type}/#{guid}")
+        response = HttpClient.get(url)
+        raise "Failed to fetch #{url}: #{response.status}" unless response.success?
+
+        Receiver.receive_public(response.body)
+      ensure
+        fetching[type].delete(guid)
+      end
+
+      # currently fetching entities in the same thread
+      private_class_method def self.fetching
+        Thread.current[:fetching_entities] ||= Hash.new {|h, k| h[k] = [] }
       end
 
       # Raised, if the entity is not fetchable
